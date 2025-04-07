9f0d0daf295951575398e959c75bde626e176dac
Vinay Badgujar <vinaybadgujar8@gmail.com>
2025-04-07 16:45:33 +0530

    add spa routing to the app

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 import Store from "./services/Store.js";
 import { loadData } from "./services/Menu.js";
+import Router from "./services/Router.js";
 
 // this allows us to what we want to use as global
 window.app = {};
 app.store = Store;
+app.router = Router;
 
 // its better to wait for the event for manipulation
 window.addEventListener("DOMContentLoaded", async () => {
   loadData();
+  app.router.init();
 });
---
+++ services/Router.js
@@ -0,0 +1,61 @@
+const Router = {
+  init: () => {
+    document.querySelectorAll("a.navlink").forEach((a) => {
+      a.addEventListener("click", (event) => {
+        event.preventDefault();
+
+        const url = event.target.getAttribute("href");
+        Router.go(url);
+      });
+    });
+
+    // event handler for url changes
+    window.addEventListener("popstate", (event) => {
+      Router.go(event.state.route, false);
+    });
+
+    // check the initial url
+    // TODO: understand this later
+    Router.go(location.pathname);
+  },
+  go: (route, addToHistory = true) => {
+    console.log(`Navigating to ${route}`);
+
+    if (addToHistory) {
+      history.pushState({ route }, null, route);
+    }
+    let pageElement = null;
+    switch (route) {
+      case "/":
+        pageElement = document.createElement("h1");
+        pageElement.textContent = "Menu";
+        break;
+      case "/order":
+        pageElement = document.createElement("h1");
+        pageElement.textContent = "Your Order";
+        break;
+      default:
+        if (route.startsWith("/product-")) {
+          pageElement = document.createElement("h1");
+          pageElement.textContent = "Product details";
+          const paramId = route.substring(route.lastIndexOf("-") + 1);
+          pageElement.dataset.id = paramId;
+          break;
+        }
+    }
+    //document.querySelector("main").children[0].remove();
+
+    if (pageElement) {
+      const cache = document.querySelector("main");
+      // we have to clear the main element: dirty way
+      cache.innerHTML = "";
+      cache.appendChild(pageElement);
+
+      // good idea to scroll to the top
+      window.scrollX = 0;
+      window.scrollY = 0;
+    }
+  },
+};
+
+export default Router;
